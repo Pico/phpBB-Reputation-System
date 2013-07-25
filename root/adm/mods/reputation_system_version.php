9871f6e9972cb35e7f45f663e9c0a0555936b2f5
Lukas <luki_9@wp.pl>
2013-07-25 21:48:21 +0200

    Change version and year

--- root/adm/mods/reputation_system_version.php
+++ root/adm/mods/reputation_system_version.php
@@ -3,7 +3,7 @@
 *
 * @package	Reputation System
 * @author	Pico88 (https://github.com/Pico88)
-* @copyright (c) 2012
+* @copyright (c) 2013
 * @license http://opensource.org/licenses/gpl-license.php GNU Public License
 *
 */
@@ -25,8 +25,8 @@ class reputation_system_version
 			'author'	=> 'Pico88',
 			'title'		=> 'Reputation System',
 			'tag'		=> 'reputation_system',
-			'version'	=> '0.6.3',
-			'file'		=> array('pico88.github.com', 'phpBB-Reputation-System', 'reputation.xml'),
+			'version'	=> '0.7.0',
+			'file'		=> array('pico88.github.io', 'phpBB-Reputation-System', 'reputation.xml'),
 		);
 	}
 }
--- root/includes/mcp/mcp_reputation.php
+++ root/includes/mcp/mcp_reputation.php
@@ -3,7 +3,7 @@
 *
 * @package	Reputation System
 * @author	Pico88 (https://github.com/Pico88)
-* @copyright (c) 2012
+* @copyright (c) 2013
 * @license http://opensource.org/licenses/gpl-license.php GNU Public License
 *
 */
